c69555c6f32f55cfc63a95d2c00006b85bc9f0e1
tyvipga410 <tranhoanglong720@gmail.com>
2021-04-04 22:42:40 +0700

    add tuan3

---
+++ Tuan3/Tuan3/frmBai18.Designer.cs
@@ -0,0 +1,177 @@
+﻿
+namespace Tuan3
+{
+    partial class frmBai18
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btndsngaunhien = new System.Windows.Forms.Button();
+            this.btnxoadangchon = new System.Windows.Forms.Button();
+            this.btnxoatenson = new System.Windows.Forms.Button();
+            this.btnxoahole = new System.Windows.Forms.Button();
+            this.btntangchu = new System.Windows.Forms.Button();
+            this.btngiamchu = new System.Windows.Forms.Button();
+            this.btnhoadautu = new System.Windows.Forms.Button();
+            this.btnxoatatca = new System.Windows.Forms.Button();
+            this.lstdanhsach = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btndsngaunhien
+            // 
+            this.btndsngaunhien.Location = new System.Drawing.Point(70, 32);
+            this.btndsngaunhien.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btndsngaunhien.Name = "btndsngaunhien";
+            this.btndsngaunhien.Size = new System.Drawing.Size(300, 43);
+            this.btndsngaunhien.TabIndex = 0;
+            this.btndsngaunhien.Text = "Nhập tên ngẫu nhiên";
+            this.btndsngaunhien.UseVisualStyleBackColor = true;
+            this.btndsngaunhien.Click += new System.EventHandler(this.btndsngaunhien_Click);
+            // 
+            // btnxoadangchon
+            // 
+            this.btnxoadangchon.Location = new System.Drawing.Point(705, 89);
+            this.btnxoadangchon.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnxoadangchon.Name = "btnxoadangchon";
+            this.btnxoadangchon.Size = new System.Drawing.Size(354, 43);
+            this.btnxoadangchon.TabIndex = 0;
+            this.btnxoadangchon.Text = "Xóa Phần tử đang chọn";
+            this.btnxoadangchon.UseVisualStyleBackColor = true;
+            this.btnxoadangchon.Click += new System.EventHandler(this.btnxoadangchon_Click);
+            // 
+            // btnxoatenson
+            // 
+            this.btnxoatenson.Location = new System.Drawing.Point(705, 142);
+            this.btnxoatenson.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnxoatenson.Name = "btnxoatenson";
+            this.btnxoatenson.Size = new System.Drawing.Size(354, 43);
+            this.btnxoatenson.TabIndex = 0;
+            this.btnxoatenson.Text = "Xóa phần tử có tên là Sơn";
+            this.btnxoatenson.UseVisualStyleBackColor = true;
+            this.btnxoatenson.Click += new System.EventHandler(this.btnxoatenson_Click);
+            // 
+            // btnxoahole
+            // 
+            this.btnxoahole.Location = new System.Drawing.Point(705, 194);
+            this.btnxoahole.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnxoahole.Name = "btnxoahole";
+            this.btnxoahole.Size = new System.Drawing.Size(354, 43);
+            this.btnxoahole.TabIndex = 0;
+            this.btnxoahole.Text = "Xóa phần tử có họ là Lê";
+            this.btnxoahole.UseVisualStyleBackColor = true;
+            this.btnxoahole.Click += new System.EventHandler(this.btnxoahole_Click);
+            // 
+            // btntangchu
+            // 
+            this.btntangchu.Location = new System.Drawing.Point(705, 246);
+            this.btntangchu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btntangchu.Name = "btntangchu";
+            this.btntangchu.Size = new System.Drawing.Size(354, 43);
+            this.btntangchu.TabIndex = 0;
+            this.btntangchu.Text = "Chuyển phần tử đang chọn thành chữ Hoa";
+            this.btntangchu.UseVisualStyleBackColor = true;
+            this.btntangchu.Click += new System.EventHandler(this.btntangchu_Click);
+            // 
+            // btngiamchu
+            // 
+            this.btngiamchu.Location = new System.Drawing.Point(705, 298);
+            this.btngiamchu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btngiamchu.Name = "btngiamchu";
+            this.btngiamchu.Size = new System.Drawing.Size(354, 43);
+            this.btngiamchu.TabIndex = 0;
+            this.btngiamchu.Text = "Chuyển phần tử đang chọn thành chữ thường";
+            this.btngiamchu.UseVisualStyleBackColor = true;
+            this.btngiamchu.Click += new System.EventHandler(this.btngiamchu_Click);
+            // 
+            // btnhoadautu
+            // 
+            this.btnhoadautu.Location = new System.Drawing.Point(705, 351);
+            this.btnhoadautu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnhoadautu.Name = "btnhoadautu";
+            this.btnhoadautu.Size = new System.Drawing.Size(354, 78);
+            this.btnhoadautu.TabIndex = 0;
+            this.btnhoadautu.Text = "Chuyển PT đang chọn thành chữ vitết Hoa đầu từ";
+            this.btnhoadautu.UseVisualStyleBackColor = true;
+            this.btnhoadautu.Click += new System.EventHandler(this.btnhoadautu_Click);
+            // 
+            // btnxoatatca
+            // 
+            this.btnxoatatca.Location = new System.Drawing.Point(705, 442);
+            this.btnxoatatca.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnxoatatca.Name = "btnxoatatca";
+            this.btnxoatatca.Size = new System.Drawing.Size(354, 43);
+            this.btnxoatatca.TabIndex = 0;
+            this.btnxoatatca.Text = "Xóa tất cả các phần tử";
+            this.btnxoatatca.UseVisualStyleBackColor = true;
+            this.btnxoatatca.Click += new System.EventHandler(this.btnxoatatca_Click);
+            // 
+            // lstdanhsach
+            // 
+            this.lstdanhsach.FormattingEnabled = true;
+            this.lstdanhsach.ItemHeight = 20;
+            this.lstdanhsach.Location = new System.Drawing.Point(72, 105);
+            this.lstdanhsach.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lstdanhsach.Name = "lstdanhsach";
+            this.lstdanhsach.Size = new System.Drawing.Size(298, 464);
+            this.lstdanhsach.TabIndex = 1;
+            // 
+            // frmBai18
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.lstdanhsach);
+            this.Controls.Add(this.btnxoatatca);
+            this.Controls.Add(this.btnhoadautu);
+            this.Controls.Add(this.btngiamchu);
+            this.Controls.Add(this.btntangchu);
+            this.Controls.Add(this.btnxoahole);
+            this.Controls.Add(this.btnxoatenson);
+            this.Controls.Add(this.btnxoadangchon);
+            this.Controls.Add(this.btndsngaunhien);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "frmBai18";
+            this.Text = "frmchuoi";
+            this.Load += new System.EventHandler(this.frmBai18_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btndsngaunhien;
+        private System.Windows.Forms.Button btnxoadangchon;
+        private System.Windows.Forms.Button btnxoatenson;
+        private System.Windows.Forms.Button btnxoahole;
+        private System.Windows.Forms.Button btntangchu;
+        private System.Windows.Forms.Button btngiamchu;
+        private System.Windows.Forms.Button btnhoadautu;
+        private System.Windows.Forms.Button btnxoatatca;
+        private System.Windows.Forms.ListBox lstdanhsach;
+    }
+}
+
---
+++ Tuan3/Tuan3/frmBai18.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tuan3
+{
+    public partial class frmBai18 : Form
+    {
+       
+        public frmBai18()
+        {
+            InitializeComponent();
+        }
+
+
+        private void frmBai18_Load(object sender, EventArgs e)
+        {                           
+        }        
+        private void btndsngaunhien_Click(object sender, EventArgs e)
+        {
+            String[] ho = { "nguyễn", "trần", "đinh", "gia", "ngô", "le", "hà", "lâm", "vương" };
+            String[] tenlot = { "thị", "hoàng anh", "huy gia", "cao đồng", "van", "duy", "thanh", "anh" };
+            String[] ten = { "lan", "huong", "tuan", "hùng", "cuong", "vi", "loan", "nam", "tú", "nga", "phát", "kiệt", "triều", "hạ", "hải","son" };
+            StringBuilder HoTen;
+            String h = "",l="",t="";           
+            Random x = new Random();
+            for(int i=0;i<10; i++)
+            {
+                HoTen = new StringBuilder();
+                h = ho[x.Next(0, ho.Length)];
+                HoTen.Append(h);
+                l = tenlot[x.Next(0, tenlot.Length)];
+                HoTen.Append(" ");
+                HoTen.Append(l);               
+                t = ten[x.Next(0, ten.Length)];
+                HoTen.Append(" ");
+                HoTen.Append(t);
+                lstdanhsach.Items.Add(HoTen.ToString());
+            }
+        }
+
+        private void btnxoadangchon_Click(object sender, EventArgs e)
+        {
+            int i = 0;
+          while(lstdanhsach.SelectedItems.Count>0)
+            {
+                i = lstdanhsach.SelectedIndices[0];
+                lstdanhsach.Items.RemoveAt(i);
+            }
+        }
+
+        private void btnxoatenson_Click(object sender, EventArgs e)
+        {          
+            String Ten = "son";
+            String HoVaTen;
+            for(int i=0;i<lstdanhsach.Items.Count;i++)
+            {
+                lstdanhsach.SetSelected(i, false);
+                HoVaTen = lstdanhsach.Items[i].ToString();
+                if (HoVaTen.StartsWith(Ten))
+                {
+                    lstdanhsach.Items.RemoveAt(i);
+                }
+            }
+        }
+
+        private void btnxoahole_Click(object sender, EventArgs e)
+        {
+            String Ho = "le";
+            String HoVaTen;
+            for (int i = 0; i < lstdanhsach.Items.Count; i++)
+            {
+                lstdanhsach.SetSelected(i, false);
+                HoVaTen = lstdanhsach.Items[i].ToString();
+                if (HoVaTen.Equals("le"))
+                {
+                    lstdanhsach.Items.RemoveAt(i);
+                }
+            }
+        }
+        string doichuhoa(string chuoi)
+        {
+            string str="";
+            for(int i=0;i<chuoi.Length;i++)
+            {
+                str += chuoi.ToString().ToUpper();
+            }
+            return str;
+        }
+        
+        private void btntangchu_Click(object sender, EventArgs e)
+        {
+            string hotenchuaxuli = "";
+            string hoten = "";
+            for (int i = 0; i < lstdanhsach.Items.Count; i++)
+            {
+                hotenchuaxuli = lstdanhsach.Items[i].ToString();
+                hoten = doichuhoa(hotenchuaxuli);
+                lstdanhsach.Items.RemoveAt(i);
+                lstdanhsach.Items.Insert(i, hoten);
+                lstdanhsach.Refresh();
+            }
+        }
+        string doihoadautu(string chuoitho)
+        {
+            string str = "";
+            for (int i = 1; i < chuoitho.Length;i++)
+            {
+                if (chuoitho[i - 1] == ' ' && chuoitho[i] != ' ')
+                    str += chuoitho.ToString().ToUpper();
+                else
+                    str += chuoitho[i];
+            }
+            str = chuoitho[0].ToString().ToUpper() + str;
+            return str;
+        }
+        private void btnhoadautu_Click(object sender, EventArgs e)
+        {
+            string hotenchuaxuli = "";
+            string hoten = "";
+            for(int i=0;i<lstdanhsach.Items.Count;i++)
+            {
+                hotenchuaxuli = lstdanhsach.Items[i].ToString();
+                hoten = doihoadautu(hotenchuaxuli);
+                lstdanhsach.Items.RemoveAt(i);
+                lstdanhsach.Items.Insert(i, hoten);
+                lstdanhsach.Refresh();
+            }
+            
+        }
+
+        private void btnxoatatca_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < lstdanhsach.Items.Count; i++)
+            {
+                lstdanhsach.Items.RemoveAt(i);
+            }
+        }
+        string doichuthuong(string chuoi)
+        {
+            string str = "";
+            for (int i = 0; i < chuoi.Length; i++)
+            {
+                str += chuoi.ToString().ToLower();
+            }
+            return str;
+        }   
+        private void btngiamchu_Click(object sender, EventArgs e)
+        {
+            string hotenchuaxuli = "";
+            string hoten = "";
+            for (int i = 0; i < lstdanhsach.Items.Count; i++)
+            {
+                hotenchuaxuli = lstdanhsach.Items[i].ToString();
+                hoten = doichuthuong(hotenchuaxuli);
+                lstdanhsach.Items.RemoveAt(i);
+                lstdanhsach.Items.Insert(i, hoten);
+                lstdanhsach.Refresh();
+            }
+        }
+    }
+}
